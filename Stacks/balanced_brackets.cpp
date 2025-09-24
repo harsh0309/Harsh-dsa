ee4840ebe678d0b32b804592f8d3d20671cb0974
harsh0309 <harsh.chandrawanshi0307@gmail.com>
2025-09-24 15:03:06 +0530

    Initial upload of isValid function for balanced Brackets

---
+++ Stacks/balanced_brackets.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+bool isValid(string str){
+    stack<char> st;
+    for(int i=0;i<str.size();i++){
+        char ch=str[i];
+        if(ch=='{' || ch=='[' || ch=='('){
+            st.push(ch);
+        }
+        else{
+            //closing bracket
+            if(ch==')' && !st.empty() && st.top()=='('){
+                st.pop();
+            }
+
+            else if(ch=='}' && !st.empty() && st.top()=='{'){
+                st.pop();
+            }
+
+            else if(ch==']' && !st.empty() && st.top()=='['){
+                st.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        }
+        return st.empty();
+    }
+int main(){
+    string str="(())";
+    cout<<isValid(str)<<endl;
+    return 0;
+}
